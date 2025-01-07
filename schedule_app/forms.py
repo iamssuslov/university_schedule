d9db22c0debad4b90ccdd1596154209196baffa8
Maxim <kurmaxim4ik@gmail.com>
2025-01-07 17:34:53 +0300

    Поправил форму авторизации и регистрации

--- schedule_app/forms.py
+++ schedule_app/forms.py
@@ -1,12 +1,21 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 User = get_user_model()
 
-
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
+
+    def clean_password1(self):
+        password = self.cleaned_data.get('password1')
+        try:
+            validate_password(password) 
+        except ValidationError as e:
+            self.add_error('password1', e)
+        return password
 
     class Meta:
         model = User
